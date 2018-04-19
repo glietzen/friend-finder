9886771fd6e613d3e49cf0074db1e59139569821
glietzen@gmail.com <glietzen@gmail.com>
2018-04-19 08:53:53 -0500

    added friends.js

---
+++ data/friends.js
@@ -0,0 +1,70 @@
+let friendsArrary = [
+    {
+        "name": "Friend 1",
+        "photo": "",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Friend 2",
+        "photo": "",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Friend 3",
+        "photo": "",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Friend 4",
+        "photo": "",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = {
+    friends
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(express.static('app/public'));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+app.listen(PORT, () => {
+    console.log(`listening ${PORT}`);
+});
