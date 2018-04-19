5e751c71a94542608bfbbf0ed6ce41cb38b2f8f9
glietzen@gmail.com <glietzen@gmail.com>
2018-04-19 15:52:45 -0500

    updated post route

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,10 +1,18 @@
-const friends = require('../data/friends');
+const friendsArrary = require('../data/friends');
 
 module.exports = (app) => {
 
     app.get("/api/friends", (req, res) => {
-        return res.json(friends);
+        return res.json(friendsArrary);
     });
+
+    app.post("/api/friends", (req, res) => {
+        let newFriend = req.body;
+        console.log(newFriend);
+        console.log(friendsArrary);
+        friendsArrary.friendsArrary.push(newFriend);
+        return res.json(friendsArrary);
+    })
 
 
 
