6873d9ce4a9e9074c9807123e559be799129a54a
glietzen@gmail.com <glietzen@gmail.com>
2018-04-19 16:51:48 -0500

    final version
    final

--- data/friends.js
+++ data/friends.js
@@ -1,7 +1,7 @@
 let friendsArrary = [
     {
-        "name": "Friend 1",
-        "photo": "",
+        "name": "Michael Scott",
+        "photo": "https://www.gq.com/story/who-is-going-to-be-back-for-the-office-revival",
         "scores": [
             5,
             1,
@@ -16,8 +16,8 @@ let friendsArrary = [
         ]
     },
     {
-        "name": "Friend 2",
-        "photo": "",
+        "name": "Dwight Schrute",
+        "photo": "https://cdn.vox-cdn.com/thumbor/mmIFrp5qe_auu_MCQhNMQszl9as=/0x0:590x393/1200x800/filters:focal(0x0:590x393)/cdn.vox-cdn.com/uploads/chorus_image/image/30606573/the-office-nbc-netflix.0.png",
         "scores": [
             5,
             1,
@@ -32,8 +32,8 @@ let friendsArrary = [
         ]
     },
     {
-        "name": "Friend 3",
-        "photo": "",
+        "name": "Kevin Malone",
+        "photo": "https://1835441770.rsc.cdn77.org/splitsider.com/wp-content/uploads/sites/2/2012/03/theoffice_kevin.jpg",
         "scores": [
             5,
             1,
@@ -48,8 +48,8 @@ let friendsArrary = [
         ]
     },
     {
-        "name": "Friend 4",
-        "photo": "",
+        "name": "Jim Halpert",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMoEpDhhlBVQCPG3_MV7K7gTTMHKb-_iMAZhdx_OKG6XJgUFsj",
         "scores": [
             5,
             1,
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,13 +7,37 @@ module.exports = (app) => {
     });
 
     app.post("/api/friends", (req, res) => {
-        let newFriend = req.body;
-        console.log(newFriend);
-        console.log(friendsArrary);
-        friendsArrary.friendsArrary.push(newFriend);
-        return res.json(friendsArrary);
-    })
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+        var userData = req.body;
+        var userScores = userData.scores;
+        var totalDifference;
 
+        for (var i = 0; i < friendsArrary.friendsArrary.length; i++) {
+            var currentFriend = friendsArrary.friendsArrary[i];
+            totalDifference = 0;
+
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+        
+        friendsArrary.friendsArrary.push(userData);
+
+        res.json(bestMatch);
+    })
 
 
 }
